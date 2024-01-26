0685eae30f93a645a1f148bd50346ab3df9ee938
Pingpong403 <pingpong440033@gmail.com>
2024-01-26 13:07:28 -0700

    added cart total

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,6 +4,16 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  // show cart total if more than 0
+  if (htmlItems.length > 0) {
+    document.querySelector("#total-label").classList.remove("hidden");
+    let total = 0;
+    cartItems.map((item) => {
+      total += item.FinalPrice;
+    });
+    document.querySelector("#cart-total").innerHTML = `$${total.toFixed(2)}`;
+  }
 }
 
 function cartItemTemplate(item) {
