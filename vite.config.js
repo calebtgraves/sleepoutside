d9c7211cea7bc8cf69251aa1ae815ba7a043cda0
brycewoodland <bwoodlan@byui.edu>
2024-02-01 15:57:22 -0700

    team3 complete

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
+  plugins: [svelte()],
   root: "src/",
 
   build: {
@@ -10,7 +12,8 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
+        productDetail: resolve(__dirname, "src/product_pages/index.html"),
       },
     },
   },
