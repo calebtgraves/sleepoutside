87674f48e225ffd36a01fc6c4874e282645f7217
calebtgraves <calebtgraves@gmail.com>
2024-02-03 11:02:49 -0700

    added alerts.json, and the alert module

---
+++ src/js/alert.mjs
@@ -0,0 +1,17 @@
+import { getData } from "./productData.mjs";
+
+getData("alerts")
+.then(alerts =>{
+    if(alerts){
+        const alertList = document.createElement("section");
+        alertList.classList.add("alert-list");
+        alerts.forEach(alert => {
+            const alertElement = document.createElement("p");
+            alertElement.innerText = alert.message;
+            alertElement.style.backgroundColor = alert.background;
+            alertElement.style.color = alert.color;
+            alertList.appendChild(alertElement);
+        });
+        document.querySelector("main").prepend(alertList);
+    }
+})
