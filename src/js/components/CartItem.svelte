cf0304fff900364a39ca147e0925e1e428e9abb9
20mthorpe <tho20019@byui.edu>
2024-02-06 16:09:38 -0700

    work on stretch, not complete!!

---
+++ src/js/components/CartItem.svelte
@@ -0,0 +1,14 @@
+<script> 
+    export let product;
+</script>
+
+<li class="product-card">
+    <a href="product_pages/index.html?product={product.Id}">
+        <img
+        src="{product.Image}"
+        alt="{product.Name}"/>
+        <h3 class="card__brand">{product.Brand.Name}</h3>
+        <h2 class="card__name">{product.NameWithoutBrand}</h2>
+        <p class="product-card__price"><span class="product-card__retail-price">{product.SuggestedRetailPrice>product.ListPrice?"$"+product.SuggestedRetailPrice.toFixed(2):""}</span> ${product.ListPrice}</p>
+    </a>
+</li>
---
+++ src/js/components/ShoppingCart.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { getData } from "../productData.mjs";
+    import CartItem from "./CartItem.svelte";
+    
+    // this is how we make a prop in svelte
+    export let category;
+    // if you are looking at this thinking that's strange to just stop with a promise
+    // you would be right.  This will make more sense in a bit...stay tuned.
+    let promise = getData(category);
+</script>
+    
+    {#await promise}
+        Loading
+    {:then products}
+    <ul class="product-list">
+    {#each products as product}
+        <CartItem product={product}/>
+    {/each}
+    </ul>
+    {/await}
